eadfa6460a25ba136557e4befaebc48e1c7fbfc8
aadiuppal <aadiuppal@gmail.com>
2014-12-28 00:35:39 -0600

    bst updates

--- c_try/3.c
+++ c_try/3.c
@@ -66,16 +66,78 @@ void postorder(struct node *root){
 	}
 }
 
+struct node * min_node(struct node *root){
+	if (root->left == NULL){
+		return root;
+	}
+	if (root->left != NULL){
+		return min_node(root->left);
+	}
+}
+
+struct node * max_node(struct node *root){
+	if(root->right == NULL){
+		return root;
+	}
+	if (root->right != NULL){
+		return max_node(root->right);
+	}
+}
+
+struct node * delete(struct node *root, int data){
+	if (data<root->data){
+		root->left=delete(root->left,data);
+	}
+	else if (data>root->data){
+		root->right=delete(root->right,data);
+		}
+	else{
+		if (root->left ==NULL){
+			struct node *temp= root->right;
+			free(root);
+			return temp;
+		}
+		else if(root->right == NULL){
+			struct node *temp= root->left;
+			free(root);
+			return temp;
+		}
+		
+		struct node *temp = max_node(root->left);
+		//struct node *temp = min_node(root->right);
+
+		root->data=temp->data;
+
+		//root->right=delete(root->right,temp->data);
+		root->left=delete(root->left,temp->data);
+	}
+	return root;
+}
+
+
+
+
 int main(){
     struct node *root = NULL;
    root= insert_node(root, 50);
     insert_node(root, 30);
     insert_node(root,60);
+    insert_node(root,90);
+  insert_node(root,55);
+	printf("inorder:\n");
     inorder(root);
-	printf("\n");
+	printf("\npreorder\n");
     preorder(root);
-	printf("\n");
+	printf("\npostorder\n");
     postorder(root);
-	printf("\n");
+	printf("search 60\n");
 printf(">>%d<<\n",search(root,60)->data);
+printf("\ndelete 60\n");
+delete(root,60);
+	printf("inorder:\n");
+    inorder(root);
+	printf("\npreorder\n");
+    preorder(root);
+	printf("\npostorder\n");
+    postorder(root);
 }
