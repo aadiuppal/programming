a15ebfdcadd57f3f76250cc6a1f68693cc582df2
aadiuppal <aadiuppal@gmail.com>
2015-01-02 02:09:40 -0600

    lak 1.4

--- c_try/1_4.c
+++ c_try/1_4.c
@@ -3,6 +3,7 @@
 void str_rep(char *str){
 	int count=0;
 	int i,j;
+	char c;
 	static char *temp;
 	temp=str;
 	while (*str != '\0'){
@@ -10,34 +11,28 @@ void str_rep(char *str){
 		*str++;
 	}
 	*str--;
-	printf("::%s::",str);
-	
 	while(*str == ' '){
 		count--;
 		*str--;
 	}
-	printf("<<%d>>",count);
 	for(i=1;i<count;i++){
 		*str--;
 	}
-	printf("::%s::\n",temp);
 	for (i=0;i<count;i++){
 		if (str[i] == ' '){
-			printf("**%d**%d\n",i,count);
 			for (j=count+1;j>i;j--){
-				printf("<<:%c:>>",temp[j]);
 				str[j]=temp[j-2];
 			}
-//			str[i]='%';
-//			str[i+1]='2';
-//			str[i+2]='0';
+			str[i]='%';
+			str[i+1]='2';
+			str[i+2]='0';
 			i=i+2;
 		}
 	}
 }
 
 int main(){
-	char *str="asd 123   ";
+	char str[]="asd 123  ";
 	printf(":%s:\n",str);
 	str_rep(str);
 	printf(":%s:\n",str);
