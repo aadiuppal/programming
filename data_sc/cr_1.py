bb233ab0ed19ffad76fe098361da5cf3c9093520
aadiuppal <aadiuppal@gmail.com>
2015-01-25 21:45:29 -0600

    reduced the clutter a bit and not needed imports

--- data_sc/cr_1.py
+++ data_sc/cr_1.py
@@ -1,18 +1,8 @@
-import urllib
-import urllib2
-import sys 
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-import re
-from random import randint
-import time
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParser
 import csv
-from selenium import webdriver
 import mechanize
 
-#"""
 count=0
 exit=0
 days=[]
@@ -23,55 +13,49 @@ days=[]
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        #print "Start tag: ",tag
-	#print "attrs: ",attrs
-        if tag == "input":
-		for attr in attrs:
-		    global count
-		    global st
-		    global days
-		    global tues
-		    global wed
-		    global golf
-		    global dinner
-		    exit=0
-		    for i in attr:
-			#print i
-			if i == "checked" and count ==0:
-				#print "tue"
-				#days.append("tuesday")
-				tues=1
-				count=1
-				exit=1
-				st=1
-			elif i == "checked" and count ==1 and exit==0:
-				#print "wed"
-				#days.append("wednesday")
-				wed=1
-				count=2
-				exit=1
-				st=1
-			elif i == "checked" and count ==2 and exit==0:
-				#print "golf"
-				#days.append("golf")
-				golf=1
-				count=3
-				exit=1
-				st=1
-			elif i == "checked" and count ==3 and exit==0:
-				#print "dinner"
-				#days.append("dinner")
-				dinner=1
-				count=4
-				exit=1
-				st=1
-		   # print days
-	           # print "     attr:", attr
+	for attr in attrs:
+	    global count
+	    global st
+	    global days
+	    global tues
+	    global wed
+	    global golf
+	    global dinner
+	    exit=0
+	    for i in attr:
+		if i == "checked" and count ==0:
+			#print "tue"
+			#days.append("tuesday")
+			tues=1
+			count=1
+			exit=1
+			st=1
+		elif i == "checked" and count ==1 and exit==0:
+			#print "wed"
+			#days.append("wednesday")
+			wed=1
+			count=2
+			exit=1
+			st=1
+		elif i == "checked" and count ==2 and exit==0:
+			#print "golf"
+			#days.append("golf")
+			golf=1
+			count=3
+			exit=1
+			st=1
+		elif i == "checked" and count ==3 and exit==0:
+			#print "dinner"
+			#days.append("dinner")
+			dinner=1
+			count=4
+			exit=1
+			st=1
+	   # print days
+
 ####################  HTML Parser class ends here #################
 
 parser=MyHTMLParser()
-#"""
-
 
 url="http://sec.tamu.edu/students/careerfair/Search.aspx?fastOps=0"
 ###############     THE URL ######################
@@ -118,12 +102,8 @@ content=response.read()
 #### form works now ###
 
 ###################  CSV generation  after reading web page ############################
-#"""
-#page=urllib2.urlopen(url)
-#content=page.read()
-#print content
+
 soup=BeautifulSoup(content,'html5lib')
-#print soup
 writer1 = csv.writer(open("comp"+".csv", 'wb'))
 writer1.writerow(["Companies","Tuesday","Wednesday","Golf","Dinner","Location"])
 num_companies=0
@@ -166,6 +146,4 @@ for entry in soup.find_all('td'):
 			golf=0
 		elif count==4:
 			dinner=0
-#print sys.getsizeof(soup)
 print num_companies
-#"""
