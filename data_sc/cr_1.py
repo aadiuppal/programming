757cf339c60e96cd7c0e4a3a284d0bb2d141b8f9
aadiuppal <aadiuppal@gmail.com>
2015-01-25 20:16:04 -0600

    removed un-necessary html parser stuff

--- data_sc/cr_1.py
+++ data_sc/cr_1.py
@@ -85,6 +85,7 @@ response = br.open(url)
 #print response.read()      # the text of the page
 response1 = br.response()  # get the response again
 #print response1.read()
+#### mechanize form debug stuff ####
 #for form in br.forms():
     #print "Form name:", form.name
     #print form
@@ -92,25 +93,29 @@ br.select_form("aspnetForm")
 #for control in br.form.controls:
     #print control
     #print "type=%s, name=%s value=%s" % (control.type, control.name, br[control.name])
+
 day_control = br.form.find_control("ctl00$ctl00$ctl00$MasterMain$Content$Content$ddlDays")
 maj_control = br.form.find_control("ctl00$ctl00$ctl00$MasterMain$Content$Content$ddlMajor")
 deg_control = br.form.find_control("ctl00$ctl00$ctl00$MasterMain$Content$Content$ddlDegree")
 emp_control = br.form.find_control("ctl00$ctl00$ctl00$MasterMain$Content$Content$ddlEmployment")
+
 #if deg_control.type == "select":  # means it's class ClientForm.SelectControl
     #for item in deg_control.items:
     	#print " name=%s values=%s" % (item.name, str([label.text  for label in item.get_labels()]))
 #print control.value
 #print control  # selected value is starred
+
 day_control.value = ["A"]
 maj_control.value = ["7"]   ##7=computer engg     8= computer science     9=electrical engg
 deg_control.value = ["M"]  ##M=masters
 emp_control.value = ["IC"] ##IC=intern /co-op
+
 #print control
 #br[control.name] = ["Tuesday"]  # equivalent and more normal
 response = br.submit()
 #print response.read()
 content=response.read()
-
+#### form works now ###
 
 ###################  CSV generation  after reading web page ############################
 #"""
@@ -121,10 +126,9 @@ soup=BeautifulSoup(content,'html5lib')
 #print soup
 writer1 = csv.writer(open("comp"+".csv", 'wb'))
 writer1.writerow(["Companies","Tuesday","Wednesday","Golf","Dinner","Location"])
-ccc=0
+num_companies=0
 for entry in soup.find_all('td'):
 	s=BeautifulSoup(str(entry))
-	#print s
 	global count
 	global st
 	global days
@@ -137,13 +141,10 @@ for entry in soup.find_all('td'):
 	if s.a:
 		if str(s.a['href']) != "/images/careerfair/ReedMap.jpg":
 			comp=s.a.contents[0]
-			ccc=ccc+1
+			num_companies=num_companies+1
 		count =0
 		this=1
-		#print days
-		#days=[]
 		if str(s.a['href']) =="/images/careerfair/ReedMap.jpg":
-			#print days
 			days.insert(0,comp)
 			days.insert(1,tues)
 			days.insert(2,wed)
@@ -154,16 +155,8 @@ for entry in soup.find_all('td'):
 			days=[]
 			st =1
 	if parser.feed(str(s)):
-		#print "<<<<<<<<<<<<<<<<<<"
-		#print str(s)
-		#print "###########"
 		parser.feed(str(s))
-		#print ">>>>>>>>>>>>>>>>>"
 	elif this==0 and st==0:
-		#print "!!!!!!!!!!!!!!"
-		#print str(s)
-		#print "@@@@@@@@@@@@@"
-		#days.append("no")
 		count =count +1
 		if count==1:
 			tues=0
@@ -174,5 +167,5 @@ for entry in soup.find_all('td'):
 		elif count==4:
 			dinner=0
 #print sys.getsizeof(soup)
-print ccc
+print num_companies
 #"""
