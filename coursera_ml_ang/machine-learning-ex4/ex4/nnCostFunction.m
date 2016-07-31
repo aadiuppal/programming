eef77fb6f03c287a1c4ed9b73cadc0d6b784d74a
Aaditya Uppal <aadiuppal@gmail.com>
2016-07-31 15:44:16 -0500

    coursera ml ex4 completed

--- coursera_ml_ang/machine-learning-ex4/ex4/nnCostFunction.m
+++ coursera_ml_ang/machine-learning-ex4/ex4/nnCostFunction.m
@@ -61,38 +61,68 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-tmp_y = zeros(size(y,1),num_labels);
-for i = 1:size(y,1),
-  tmp_y(i,y(i)) = 1;
-end
+%tmp_y = zeros(size(y,1),num_labels);
+%for i = 1:size(y,1),
+%  tmp_y(i,y(i)) = 1;
+%end
+%
+%X = [1+zeros(size(X,1),1) X];
+%a2 = sigmoid(Theta1*X');
+%a2 = a2';
+%a2 = [1+zeros(size(a2,1),1) a2];
+%h = sigmoid(Theta2*a2');
+%h = h';
+%%size(tmp_y)
+%%size(h)
+%J = sum(((-1*tmp_y).*log(h))-((1-tmp_y).*log(1-h)));
+%J= sum(J);
+%%size(J)
+%J = J/m;
+%tmp1 = Theta1(:,2:1:size(Theta1,2));
+%tmp2 = Theta2(:,2:1:size(Theta2,2));
+%J = J + ((lambda/(2*m)) * (sum(sum((tmp1.*tmp1))) + sum(sum(tmp2.*tmp2))));
+%
+%delta3 = h-tmp_y;
+%tmp_delta3 = [ones(m,1) delta3];
+%delta2 = (delta3*Theta2).*sigmoidGradient(tmp_delta3);
+%delta2 = delta2(:,2:end);
+%Theta1_grad = delta3'*a2;
+%Theta2_grad = delta2'*X;
+%size(Theta1_grad)
+%size(Theta2_grad)
 
-X = [1+zeros(size(X,1),1) X];
-a2 = sigmoid(Theta1*X');
-a2 = a2';
-a2 = [1+zeros(size(a2,1),1) a2];
-h = sigmoid(Theta2*a2');
-h = h';
-%size(tmp_y)
-%size(h)
-J = sum(((-1*tmp_y).*log(h))-((1-tmp_y).*log(1-h)));
-J= sum(J);
-%size(J)
-J = J/m;
-tmp1 = Theta1(:,2:1:size(Theta1,2));
-tmp2 = Theta2(:,2:1:size(Theta2,2));
-J = J + ((lambda/(2*m)) * (sum(sum((tmp1.*tmp1))) + sum(sum(tmp2.*tmp2))));
+X = [ones(m, 1) X];
+yd = eye(num_labels);
+y = yd(y,:);
+a1=X;
+z2=X*Theta1';
+a2=sigmoid(z2);
 
+a2=[ones(m, 1) a2];
+z3=a2*Theta2';
+a3=sigmoid(z3);
 
+logisf=(-y).*log(a3)-(1-y).*log(1-a3); % Becos y is now a matrix, so use dot product, unlike above
 
+Theta1s=Theta1(:,2:end);
+Theta2s=Theta2(:,2:end);
+J=((1/m).*sum(sum(logisf)))+(lambda/(2*m)).*(sum(sum(Theta1s.^2))+sum(sum(Theta2s.^2)));
 
+% Set all the D to zeros
+tridelta_1=0;
+tridelta_2=0;
 
-
-
-
-
-
-
-
+% Compute delta, tridelta and big D
+delta_3=a3-y;
+z2=[ones(m,1) z2];
+delta_2=delta_3*Theta2.*sigmoidGradient(z2);
+delta_2=delta_2(:,2:end);
+tridelta_1=tridelta_1+delta_2'*a1;
+tridelta_2=tridelta_2+delta_3'*a2;
+Theta1_grad=(1/m).*tridelta_1;
+Theta2_grad=(1/m).*tridelta_2;
+Theta1_grad(:, 2:input_layer_size+1) = Theta1_grad(:, 2:input_layer_size+1) + lambda / m * Theta1(:, 2:input_layer_size+1);
+Theta2_grad(:, 2:hidden_layer_size+1) = Theta2_grad(:, 2:hidden_layer_size+1) + lambda / m * Theta2(:, 2:hidden_layer_size+1);
 
 % -------------------------------------------------------------
 
