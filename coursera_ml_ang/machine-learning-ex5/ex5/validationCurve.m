89e03cdff5151b709dd7c01589a000127b1c87ef
Aaditya Uppal <aadiuppal@gmail.com>
2016-08-02 00:07:14 -0500

    ex5 ml coursera completed

--- coursera_ml_ang/machine-learning-ex5/ex5/validationCurve.m
+++ coursera_ml_ang/machine-learning-ex5/ex5/validationCurve.m
@@ -38,11 +38,12 @@ error_val = zeros(length(lambda_vec), 1);
 %       end
 %
 %
-
-
-
-
-
+for i=1:length(lambda_vec),
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X,y,lambda);
+  [error_train(i),tmp] = linearRegCostFunction(X,y,theta,0);
+  [error_val(i),tmp] = linearRegCostFunction(Xval,yval,theta,0);
+end
 
 
 
